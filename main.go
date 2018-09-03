7cf37840694e158404655fc2340dec90acaa6f4e
kojitaniguchi <tanigu0511@gmail.com>
2018-09-03 13:13:32 +0900

    heroku deploy v7

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"kyotohack2018-api/router"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	router := router.GetRouter()
 	router.Run(":8081")
 }
